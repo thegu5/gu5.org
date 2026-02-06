cca47086ff8fab2f2c7cde4e8b3ca035fc9435ae
Gus <58223632+thegu5@users.noreply.github.com>
2026-02-05 20:53:32 -0500

    misc cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
