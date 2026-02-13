97c0467f877b9b38b38ed608944fe885aa9766ba
Gus <58223632+thegu5@users.noreply.github.com>
2026-02-13 15:44:27 -0500

    icon experiment

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import expressiveCode from 'astro-expressive-code';
+import icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
     themes: ["catppuccin-mocha", "catppuccin-latte"]
   })],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), icons({ compiler: "astro" })],
 
     build: {
       assetsInlineLimit: 25 * 1024
