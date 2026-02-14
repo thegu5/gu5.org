0b3a05b2c383507485fdd37edcb55a20163538a6
Gus <58223632+thegu5@users.noreply.github.com>
2026-02-13 23:19:54 -0500

    Revert "icon experiment"
    This reverts commit 97c0467f877b9b38b38ed608944fe885aa9766ba.

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import expressiveCode from 'astro-expressive-code';
-import icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
     themes: ["catppuccin-mocha", "catppuccin-latte"]
   })],
   vite: {
-    plugins: [tailwindcss(), icons({ compiler: "astro" })],
+    plugins: [tailwindcss()],
 
     build: {
       assetsInlineLimit: 25 * 1024
