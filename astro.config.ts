296f7d6fa25fd0d7f7595a0b5176752d6e538733
Gus <58223632+thegu5@users.noreply.github.com>
2026-05-31 22:10:20 -0400

    astro 6.4, satteri for markdown

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,29 @@
+import { satteri } from "@astrojs/markdown-satteri";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import expressiveCode from "astro-expressive-code";
+import expressiveCode from "satteri-expressive-code";
 import icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gu5.org",
 
-	integrations: [
-		sitemap(),
-		expressiveCode({
-			themes: ["catppuccin-mocha", "catppuccin-latte"],
-			useThemedSelectionColors: true,
-			// reduces time until full pages are rendered
-			emitExternalStylesheet: false
+	integrations: [sitemap()],
+
+	markdown: {
+		syntaxHighlight: false, // disable shiki
+		processor: satteri({
+			features: { directive: true },
+			hastPlugins: [
+				expressiveCode({
+					themes: ["catppuccin-mocha", "catppuccin-latte"],
+					useThemedSelectionColors: true,
+				}),
+			],
 		}),
-	],
+	},
+
 	vite: {
 		plugins: [tailwindcss(), icons({ compiler: "astro" })],
 
