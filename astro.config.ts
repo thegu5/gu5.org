99c25db8a73841b4f6f8176aa4c885029185c672
Gus <58223632+thegu5@users.noreply.github.com>
2026-06-13 19:40:04 -0700

    bump deps, add astro-scute

--- astro.config.ts
+++ astro.config.ts
@@ -2,25 +2,27 @@ import { satteri } from "@astrojs/markdown-satteri";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import expressiveCode from "satteri-expressive-code";
+import expressiveCode from "astro-expressive-code";
+import scute from "astro-scute";
 import icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gu5.org",
 
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+		expressiveCode({
+			themes: ["catppuccin-mocha", "catppuccin-latte"],
+			useThemedSelectionColors: true,
+		}),
+		scute(),
+	],
 
 	markdown: {
 		syntaxHighlight: false, // disable shiki
 		processor: satteri({
 			features: { directive: true },
-			hastPlugins: [
-				expressiveCode({
-					themes: ["catppuccin-mocha", "catppuccin-latte"],
-					useThemedSelectionColors: true,
-				}),
-			],
 		}),
 	},
 
---
+++ scute.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "astro-scute";
+
+export default defineConfig({
+	identity: "did:plc:bqqamzqieuyniwrpcosasyod",
+	publications: [
+		{
+			collectionName: "blog",
+			record: {
+				$type: "site.standard.publication",
+				name: "Gus's site",
+				url: "https://gu5.org",
+				basicTheme: {
+					$type: "site.standard.theme.basic",
+					accent: {
+						r: 255,
+						g: 255,
+						b: 255,
+						$type: "site.standard.theme.color#rgb",
+					},
+					background: {
+						$type: "site.standard.theme.color#rgb",
+						r: 49,
+						g: 65,
+						b: 88,
+					},
+					foreground: {
+						$type: "site.standard.theme.color#rgb",
+						r: 209,
+						g: 213,
+						b: 220,
+					},
+					accentForeground: {
+						$type: "site.standard.theme.color#rgb",
+						r: 0,
+						g: 0,
+						b: 0,
+					},
+				},
+				preferences: {
+					showInDiscover: true,
+				},
+			},
+			baseContentPath: "/blog",
+		},
+	],
+});
